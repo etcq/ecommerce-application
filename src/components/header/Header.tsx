feb95bb4bc9913aa28e3fb6ca8a0c98b55264542
etcq <resonans93@gmail.com>
2025-05-05 16:33:25 +0500

    feat: add login menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,29 @@
-import LoginMenu from './login-menu/LoginMenu';
-
-import styles from './header.module.scss';
-import logoImg from '../../assets/images/header/logo.png';
 import { NavLink } from 'react-router';
 
+import LoginMenu from './login-menu/LoginMenu';
+import logoImg from '../../assets/images/header/logo.png';
+
+import styles from './header.module.scss';
+import userImg from '../../assets/images/header/user.svg';
+import bucketImg from '../../assets/images/header/bucket.svg';
+import { useState } from 'react';
+
 export default function Header(): React.JSX.Element {
+  const [userMenuStatus, setUserMenuStatus] = useState(false);
+
+  // const closeLoginMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   console.log(event.target);
+  //   if (event.target instanceof HTMLElement) {
+  //     if (!event.target.classList.contains('login-menu')) {
+  //       setUserMenuStatus('close');
+  //     }
+  //   }
+  // };
+
+  const toggleLoginMenu = () => {
+    setUserMenuStatus(!userMenuStatus);
+  };
+
   return (
     <div className="container">
       <header className={styles.header}>
@@ -27,10 +46,16 @@ export default function Header(): React.JSX.Element {
           </li>
         </ul>
         <div className={styles.header__user}>
-          <div className={styles['header__user-login']}></div>
-          <div className={styles['header__user-bucket']}></div>
+          <div className={styles['header__user-wrapper']}>
+            <div className={styles['header__user-icon']} onClick={() => toggleLoginMenu()}>
+              <img src={userImg} />
+            </div>
+            <LoginMenu userMenuStatus={userMenuStatus} />
+          </div>
+          <div className={styles['header__user-icon']}>
+            <img src={bucketImg} />
+          </div>
         </div>
-        <LoginMenu />
       </header>
     </div>
   );
--- src/components/header/login-menu/LoginMenu.tsx
+++ src/components/header/login-menu/LoginMenu.tsx
@@ -1,9 +1,23 @@
-import './_login-menu.scss';
+import styles from './_login-menu.module.scss';
+import { useEffect } from 'react';
+interface loginMenuProps {
+  userMenuStatus: boolean;
+  // closeMenu: (event: React.MouseEvent<HTMLElement>) => void;
+}
 
-export default function LoginMenu(): React.JSX.Element {
+export default function LoginMenu(props: loginMenuProps): React.JSX.Element {
+  const changeClass = (): string => {
+    return props.userMenuStatus ? 'open' : 'close';
+  };
+
+  useEffect(() => {
+    changeClass();
+  });
+
   return (
-    <div className="login-menu hidden">
-      <div className="login-menu__title">
+    <div className={`${styles['login-menu']} ${styles[changeClass()]}`}>
+      <div className={styles['login-menu__head']}></div>
+      <div className={styles['login-menu__title']}>
         <h3>Login please</h3>
       </div>
       <button type="button" className="login-menu__login">
