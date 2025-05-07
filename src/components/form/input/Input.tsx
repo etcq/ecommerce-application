76d161be4e152e89dbb5c3f1fc87f18b26e380fd
turik777 <52492191+turik777@users.noreply.github.com>
2025-05-07 18:12:33 +0300

    feat: add button and input components (#4)
    * feat: add button and input components
    
    * refactor: change components logic and add button variants
    
    * refactor: rename prop and restrict values

---
+++ src/components/form/input/Input.tsx
@@ -0,0 +1,27 @@
+import styles from './input.module.scss';
+import { FC } from 'react';
+
+interface IProps {
+  type?: 'text' | 'email' | 'password' | 'date';
+  label?: string;
+  id?: string;
+  placeholder?: string;
+  onChange?: () => void;
+  wrapperClassName: string;
+}
+
+const Input: FC<IProps> = ({ type = 'text', label, id, placeholder, onChange, wrapperClassName }) => {
+  return (
+    <div className={`${styles.wrapper} ${styles[wrapperClassName]}`}>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+
+      <input className={styles.input} type={type} id={id} placeholder={placeholder} onChange={onChange}></input>
+    </div>
+  );
+};
+
+export default Input;
