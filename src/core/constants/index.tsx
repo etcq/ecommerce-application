f8b396bcccea14beceed217bf2a6e25948b6453c
ill_Nino <ill_Nino@bk.ru>
2025-05-07 21:14:06 +0300

    feat(constants): add constants

---
+++ src/core/constants/index.tsx
@@ -0,0 +1,8 @@
+export enum CartSignInModeEnum {
+  MergeWithExistingCustomerCart = 'MergeWithExistingCustomerCart',
+}
+
+export enum LocalStorageKeys {
+  TOKEN = 'ctp-auth-token',
+  ANONYMOUSCARTID = 'anonymousCartId',
+}
