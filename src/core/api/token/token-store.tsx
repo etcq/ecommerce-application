f8f61fe663655b4aa9550bcc03b48e4e2e75098a
ill_Nino <ill_Nino@bk.ru>
2025-05-07 20:51:49 +0300

    feat(api): add token-store.tsx

---
+++ src/core/api/token/token-store.tsx
@@ -0,0 +1,49 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import type { TokenCache, TokenStore } from '@commercetools/sdk-client-v2';
+import { LocalStorageKeys} from "@/core/constants";
+
+interface ITokenCacheState extends TokenStore {
+  setTokenData: (newTokenData: TokenStore) => void;
+  clearTokenData: () => void;
+}
+
+export const useTokenCacheStore = create<ITokenCacheState>()(
+  persist(
+    (set) => ({
+      token: '',
+      refreshToken: '',
+      expirationTime: 0,
+      setTokenData: (newTokenData: TokenStore): void => set(newTokenData),
+      clearTokenData: (): void => set({ token: '', refreshToken: '', expirationTime: 0 }),
+    }),
+    {
+      name: LocalStorageKeys.TOKEN,
+      storage: createJSONStorage((): Storage => localStorage),
+    },
+  ),
+);
+
+export const tokenCache: TokenCache & {
+  clear: () => void;
+  isTokenExpired: () => boolean;
+} = {
+  get: () => {
+    const state: ITokenCacheState = useTokenCacheStore.getState();
+    return {
+      token: state.token,
+      refreshToken: state.refreshToken,
+      expirationTime: state.expirationTime,
+    };
+  },
+  set: (data: TokenStore): void => {
+    useTokenCacheStore.getState().setTokenData(data);
+  },
+  clear: (): void => {
+    useTokenCacheStore.getState().clearTokenData();
+  },
+  isTokenExpired: (): boolean => {
+    const { expirationTime } = useTokenCacheStore.getState();
+    return expirationTime !== 0 && expirationTime <= Date.now();
+  },
+};
