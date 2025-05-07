e8e40b578a73dc8b3521e0197b7ba8ef101a8829
ill_Nino <ill_Nino@bk.ru>
2025-05-07 20:52:20 +0300

    feat(api): add with-password-flow.tsx

---
+++ src/core/api/middlewere/password-flow.ts
@@ -0,0 +1,45 @@
+import {
+  ClientBuilder,
+  Client,
+  type PasswordAuthMiddlewareOptions,
+  type HttpMiddlewareOptions,
+  type TokenCache,
+} from '@commercetools/sdk-client-v2';
+import { createApiBuilderFromCtpClient, type ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk';
+
+export const withPasswordFlow = (
+  username: string,
+  password: string,
+  tokenCache: TokenCache,
+): ByProjectKeyRequestBuilder | null => {
+  const projectKey: string = import.meta.env.VITE_CTP_PROJECT_KEY;
+
+  const authOptions: PasswordAuthMiddlewareOptions = {
+    host: import.meta.env.VITE_CTP_AUTH_URL,
+    projectKey: projectKey,
+    credentials: {
+      clientId: import.meta.env.VITE_CTP_CLIENT_ID,
+      clientSecret: import.meta.env.VITE_CTP_CLIENT_SECRET,
+      user: {
+        username,
+        password,
+      },
+    },
+    scopes: [import.meta.env.VITE_CTP_SCOPES],
+    tokenCache,
+    fetch,
+  };
+
+  const httpOptions: HttpMiddlewareOptions = {
+    host: import.meta.env.VITE_CTP_API_URL,
+    fetch,
+  };
+
+  const ctpClient: Client = new ClientBuilder()
+    .withProjectKey(projectKey)
+    .withPasswordFlow(authOptions)
+    .withHttpMiddleware(httpOptions)
+    .build();
+
+  return createApiBuilderFromCtpClient(ctpClient).withProjectKey({ projectKey: projectKey });
+};
