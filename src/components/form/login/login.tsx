95facdb2ae72bc18fc41b62b82b09c4a3dbc4e65
aQafresca <124169157+aQafresca@users.noreply.github.com>
2025-05-11 20:37:52 +0300

    Add login page (#7)
    * feat(project): add SDK dependencies
    
    * feat(gitignore): add env to gitignore
    
    * feat(project): add env.exemple file
    
    * feat(api): add client-build.tsx
    
    * feat(api): add token-store.tsx
    
    * feat(api): add with-password-flow.tsx
    
    * feat(api): add create-customers api
    
    * feat(api): add anonymous-session-flow.tsx
    
    * feat(api): add refresh-token-flow.tsx
    
    * feat(api): add login.tsx
    
    * feat(constants): add constants
    
    * refactor(token): move ITokenCacheState to interfaces.ts
    
    * feat(hooks): add authState hook
    
    * refactor(login-page): update auth store end move to store folder
    
    * feat(login-page): add validation-scheme.tsx end validation.tsx
    
    * feat(style): add scss properties
    
    * feat(project): add dependencies
    
    * refactor(file-name): change password-flow.tsx
    
    * feat(login-page): add loginForm end LoginPage.tsx
    
    * feat(login-page): add login img
    
    * fix: eslint error
    
    * refactor(login-page): rework file extension

---
+++ src/components/form/login/login.tsx
@@ -0,0 +1,112 @@
+import styles from './loginForm.module.scss';
+import { Controller, useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Input from '@components/form/input/Input.tsx';
+import Button from '@components/button/Button.tsx';
+import { useAuthStore } from '@/core/stores/use-auth-state.ts';
+import * as React from 'react';
+import { loginFormSchema } from '@components/form/login/validation-scheme.ts';
+import { FormEvent } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router';
+import { ROUTES } from '@/constants/constants.ts';
+
+type TLoginFormValues = z.infer<typeof loginFormSchema>;
+
+export const LoginForm: React.FC = () => {
+  const [apiError, setApiError] = React.useState<string | null>(null);
+  const navigate: NavigateFunction = useNavigate();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<TLoginFormValues>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const { login } = useAuthStore();
+  const goToRegistration = () => {
+    void navigate(ROUTES.REGISTRATION);
+  };
+
+  const onSubmit = async (data: TLoginFormValues): Promise<void> => {
+    setApiError(null);
+    try {
+      await login(data.email.toLowerCase(), data.password);
+      void navigate(ROUTES.MAIN);
+    } catch (error) {
+      if (error instanceof Error) {
+        setApiError(error.message);
+      }
+    }
+  };
+
+  return (
+    <>
+      <h2>LOGIN</h2>
+      <p>
+        Do not have an account,{' '}
+        <span className={styles.link} onClick={goToRegistration}>
+          create a new one.
+        </span>
+      </p>
+
+      <form
+        className={styles.wrapper}
+        onSubmit={(e: FormEvent<HTMLFormElement>): void => {
+          e.preventDefault();
+          void handleSubmit(onSubmit)();
+        }}
+      >
+        <div className={styles.container}>
+          <Controller
+            name={'email'}
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                type={'email'}
+                label={'Email'}
+                id={'login-form__email'}
+                placeholder={'email'}
+                wrapperClassName={'wrapper'}
+              />
+            )}
+          />
+          {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+        </div>
+        <div className={styles.container}>
+          <Controller
+            name={'password'}
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                type={'password'}
+                id={'login-form__password'}
+                label={'Password'}
+                placeholder={'password'}
+                wrapperClassName={'wrapper'}
+              />
+            )}
+          />
+          {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+        </div>
+
+        {apiError && <span className={styles.error}>{apiError}</span>}
+
+        <Button
+          className={styles.button}
+          size={'medium'}
+          type={'submit'}
+          children={'Log In'}
+          disabled={isSubmitting}
+        ></Button>
+      </form>
+    </>
+  );
+};
---
+++ src/core/api/middlewere/refresh-token-flow.ts
@@ -0,0 +1,33 @@
+import type { HttpMiddlewareOptions, RefreshAuthMiddlewareOptions } from '@commercetools/sdk-client-v2';
+import { ClientBuilder, Client } from '@commercetools/sdk-client-v2';
+import { ByProjectKeyRequestBuilder, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { tokenCache } from '@/core/api/token/token-store.ts';
+
+export function withRefreshTokenFlow(refreshToken: string): ByProjectKeyRequestBuilder {
+  const projectKey = String(import.meta.env.VITE_CTP_PROJECT_KEY);
+  const options: RefreshAuthMiddlewareOptions = {
+    host: String(import.meta.env.VITE_CTP_AUTH_URL),
+    projectKey: projectKey,
+    credentials: {
+      clientId: String(import.meta.env.VITE_CTP_CLIENT_ID),
+      clientSecret: String(import.meta.env.VITE_CTP_CLIENT_SECRET),
+    },
+    refreshToken,
+    tokenCache,
+    fetch,
+  };
+  const httpOptions: HttpMiddlewareOptions = {
+    host: String(import.meta.env.VITE_CTP_API_URL),
+    fetch,
+  };
+
+  const ctpClient: Client = new ClientBuilder()
+    .withProjectKey(projectKey)
+    .withRefreshTokenFlow(options)
+    .withHttpMiddleware(httpOptions)
+    .build();
+
+  return createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+    projectKey: projectKey,
+  });
+}
