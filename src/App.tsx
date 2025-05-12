ee38f8d83b0a02c8452f771259ab9f47db1c6555
etcq <resonans93@gmail.com>
2025-05-12 23:10:18 +0500

    feat: add protected routing, add initAuth to reload page

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,16 @@ import RegistrationPage from './pages/user/registration/RegistrationPage';
 import { ROUTES } from './constants/constants';
 import Bucket from './pages/bucket/Bucket';
 import { ProtectedRoutesForAuth } from './core/routes/protected-routes';
+import { useAuthStore } from './core/stores/use-auth-state';
+import { useEffect } from 'react';
 
 function App() {
+  const { initializationAuth } = useAuthStore();
+
+  useEffect(() => {
+    initializationAuth();
+  }, [initializationAuth]);
+
   return (
     <BrowserRouter>
       <>
@@ -19,10 +27,10 @@ function App() {
           <Route index element={<MainPage />} />
           <Route path={ROUTES.ABOUT} element={<AboutPage />} />
           <Route path={ROUTES.PRODUCT_LIST} element={<ProductList />} />
-          {/* <Route element={<ProtectedRoutesForAuth />}> */}
+          <Route element={<ProtectedRoutesForAuth />}>
             <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-          {/* </Route> */}
-          <Route path={ROUTES.REGISTRATION} element={<RegistrationPage />} />
+            <Route path={ROUTES.REGISTRATION} element={<RegistrationPage />} />
+          </Route>
           <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
           <Route path={ROUTES.BUCKET} element={<Bucket />} />
         </Routes>
