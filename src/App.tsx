c3e9c16cf88cf678fbe2650ebd2b31f9fd3aeae8
etcq <resonans93@gmail.com>
2025-05-13 15:18:48 +0500

    feat: add a 404 component and change the routing for it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainPage from './pages/main/Main';
 import ProductList from './pages/products/product-list/ProductList';
 import Header from './components/header/Header';
 import LoginPage from './pages/user/login/LoginPage';
+import NotFoundPage from './pages/not-found/NotFoundPage';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import ProfilePage from './pages/user/profile/Profile';
 import RegistrationPage from './pages/user/registration/RegistrationPage';
@@ -11,7 +12,6 @@ import Bucket from './pages/bucket/Bucket';
 import { ProtectedRoutesForAuth } from './core/routes/protected-routes';
 import { useAuthStore } from './core/stores/use-auth-state';
 import { useEffect } from 'react';
-
 function App() {
   const { initializationAuth } = useAuthStore();
 
@@ -23,17 +23,20 @@ function App() {
     <BrowserRouter>
       <>
         <Header />
-        <Routes>
-          <Route index element={<MainPage />} />
-          <Route path={ROUTES.ABOUT} element={<AboutPage />} />
-          <Route path={ROUTES.PRODUCT_LIST} element={<ProductList />} />
-          <Route element={<ProtectedRoutesForAuth />}>
-            <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-            <Route path={ROUTES.REGISTRATION} element={<RegistrationPage />} />
-          </Route>
-          <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
-          <Route path={ROUTES.BUCKET} element={<Bucket />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route index element={<MainPage />} />
+            <Route path={ROUTES.ABOUT} element={<AboutPage />} />
+            <Route path={ROUTES.PRODUCT_LIST} element={<ProductList />} />
+            <Route element={<ProtectedRoutesForAuth />}>
+              <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+              <Route path={ROUTES.REGISTRATION} element={<RegistrationPage />} />
+            </Route>
+            <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+            <Route path={ROUTES.BUCKET} element={<Bucket />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
       </>
     </BrowserRouter>
   );
---
+++ src/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,30 @@
+import Button from '@/components/button/Button';
+import imgPath from '@assets/images/login/login-sneakers.jpg';
+import styles from './not-found.module.scss';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '@/constants/constants';
+
+export default function NotFoundPage(): React.JSX.Element {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className={styles['not-found']}>
+        <img src={imgPath} className={styles['not-found_img']} />
+        <div className={styles['not-found_description']}>
+          <h2 className={styles.title}>Oh... Sh#!%t</h2>
+          <h2 className={styles.title}>Page is not found</h2>
+          <p>We can't find the page you were looking for.</p>
+          <Button
+            type="button"
+            size="large"
+            children="Back to main page"
+            onClick={() => {
+              void navigate(ROUTES.MAIN);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
