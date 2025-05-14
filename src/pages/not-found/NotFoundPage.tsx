50158348748c82d1ee6eda78be3520170175ade3
etcq <resonans93@gmail.com>
2025-05-14 19:16:31 +0500

    feat: change 404 img

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/button/Button';
-import imgPath from '@assets/images/login/login-sneakers.jpg';
+import imgPath from '@assets/images/not-found/not-found.png';
 import styles from './not-found.module.scss';
 import { useNavigate } from 'react-router';
 import { ROUTES } from '@/constants/constants';
