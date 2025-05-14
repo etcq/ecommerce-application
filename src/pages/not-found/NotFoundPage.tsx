e5ab791c6fb30f4e7f09f710b2fef74f62721a07
etcq <resonans93@gmail.com>
2025-05-15 01:10:40 +0500

    refactor: delete unused div

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -8,22 +8,20 @@ export default function NotFoundPage(): React.JSX.Element {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div className={styles['not-found']}>
-        <img src={imgPath} className={styles['not-found_img']} alt="Sad sneaker" />
-        <div className={styles['not-found_description']}>
-          <h2 className={styles.title}>Error 404</h2>
-          <h2 className={styles.title}>Page is not found</h2>
-          <p>We can't find the page you were looking for.</p>
-          <Button
-            type="button"
-            size="large"
-            children="Back to main page"
-            onClick={() => {
-              void navigate(ROUTES.MAIN);
-            }}
-          />
-        </div>
+    <div className={styles['not-found']}>
+      <img src={imgPath} className={styles['not-found_img']} alt="Sad sneaker" />
+      <div className={styles['not-found_description']}>
+        <h2 className={styles.title}>Error 404</h2>
+        <h2 className={styles.title}>Page is not found</h2>
+        <p>We can't find the page you were looking for.</p>
+        <Button
+          type="button"
+          size="large"
+          children="Back to main page"
+          onClick={() => {
+            void navigate(ROUTES.MAIN);
+          }}
+        />
       </div>
     </div>
   );
