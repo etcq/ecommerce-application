d93a5eae6ef1008495262d90daa421f5f1076780
etcq <resonans93@gmail.com>
2025-05-20 05:30:31 +0500

    feat: add useLocation hook for Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,15 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Header from '../header/Header';
 import { Suspense } from 'react';
 import Loading from '../loading/Loading';
 
 export default function Layout(): React.JSX.Element {
+  const location = useLocation();
   return (
     <>
       <Header />
       <main>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading />} key={location.key}>
           <Outlet />
         </Suspense>
       </main>
