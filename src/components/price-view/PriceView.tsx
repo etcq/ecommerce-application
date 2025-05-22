d9b7196371c991a6f6535268aa26cce1c39f0827
etcq <resonans93@gmail.com>
2025-05-22 07:16:10 +0500

    feat: add discount percent view

--- src/components/price-view/PriceView.tsx
+++ src/components/price-view/PriceView.tsx
@@ -7,8 +7,14 @@ export default function PriceView(props: Pick<IProductInfoForCard, 'prices'>): J
   const formatPrice = (price: number) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price / 100);
   };
+  const getPercentOfDiscount = () => {
+    if (discount) {
+      return ((main - discount) / main) * 100;
+    }
+  };
   return (
     <div className={styles['price-wrapper']}>
+      {discount && <div className={styles['price-discount-percent']}>-{getPercentOfDiscount()}%</div>}
       <div className={`${styles.price} ${discount && styles.discounted}`}>
         {main === 0 ? 'The product is not available' : formatPrice(main)}
       </div>
