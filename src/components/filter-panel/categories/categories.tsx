64f455b4ab4320f6c0b5b3ae352c68e2b3c3a284
aQafresca <124169157+aQafresca@users.noreply.github.com>
2025-06-02 21:34:36 +0300

    fix(components): Prevent category navigation state reset (#44)

--- src/components/filter-panel/categories/categories.tsx
+++ src/components/filter-panel/categories/categories.tsx
@@ -35,10 +35,6 @@ export function CategoriesNavigation() {
       }
     };
     void fetchCategories();
-
-    return (): void => {
-      setReset();
-    };
   }, [setReset, setAllCategories]);
 
   const handleRootCategoryClick = (name: string): void => {
