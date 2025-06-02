0857f2afbdf32643f6a374ac739085ea2aeb93dc
turik777 <52492191+turik777@users.noreply.github.com>
2025-06-02 21:10:26 +0300

    feat: add tests for user page (#41)
    * feat: add tests for user page
    
    * feat: add tests for password page
    
    * refactor: add function for router render

---
+++ src/test/components/Password.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { BrowserRouter } from 'react-router';
+import PasswordForm from '@/components/form/user/password/Password';
+
+const renderWithRouter = (ui: React.ReactElement) => render(<BrowserRouter>{ui}</BrowserRouter>);
+
+describe('Password component', () => {
+  it('password form elements', () => {
+    renderWithRouter(<PasswordForm />);
+    expect(screen.getByLabelText('Current Password')).toBeInTheDocument();
+    expect(screen.getByLabelText('New Password')).toBeInTheDocument();
+    expect(screen.getByLabelText('Confirm New Password')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /save changes/i })).toBeInTheDocument();
+  });
+
+  it('displays error when new password and confirmation do not match', async () => {
+    renderWithRouter(<PasswordForm />);
+
+    const newPassword = screen.getByLabelText('New Password');
+    const newPasswordConfirm = screen.getByLabelText('Confirm New Password');
+    fireEvent.change(newPassword, { target: { value: 'Password123' } });
+    fireEvent.change(newPasswordConfirm, { target: { value: 'Password1234' } });
+
+    expect(await screen.findByText('Passwords do not match')).toBeInTheDocument();
+  });
+});
