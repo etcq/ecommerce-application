3b755574040d2a49afa8835bd4a602dba7eb22fe
ill_Nino <innxfect@gmail.com>
2025-06-18 21:49:56 +0300

    fix(api): invalid token

--- src/core/api/customers/login.ts
+++ src/core/api/customers/login.ts
@@ -1,6 +1,5 @@
 import { tokenCache } from '@/core/api/token/token-store.ts';
 import { withPasswordFlow } from '@/core/api/middlewere/password-flow.ts';
-import { withRefreshTokenFlow } from '@/core/api/middlewere/refresh-token-flow.ts';
 import { CartSignInModeEnum, LocalStorageKeys } from '@/constants/constants';
 import { ByProjectKeyRequestBuilder, CustomerSignInResult } from '@commercetools/platform-sdk';
 import { ClientResponse } from '@commercetools/ts-client';
@@ -9,23 +8,6 @@ import { getActiveCart } from '@/core/api/cart/get-active-cart.ts';
 export async function loginCustomers(email: string, password: string): Promise<CustomerSignInResult | null> {
   try {
     const anonymousCartId: string | null = localStorage.getItem(LocalStorageKeys.ANONYMOUS_CART_ID);
-    const refreshToken: string | undefined = tokenCache.get().refreshToken;
-
-    if (anonymousCartId && refreshToken) {
-      const refreshTokenClient: ByProjectKeyRequestBuilder = withRefreshTokenFlow(refreshToken);
-      await refreshTokenClient
-        .me()
-        .login()
-        .post({
-          body: {
-            email,
-            password,
-            activeCartSignInMode: CartSignInModeEnum.MergeWithExistingCustomerCart,
-            updateProductData: true,
-          },
-        })
-        .execute();
-    }
 
     tokenCache.clear();
 
@@ -34,29 +16,47 @@ export async function loginCustomers(email: string, password: string): Promise<C
     if (!passwordFlowClient) {
       return null;
     }
+
+    const data: {
+      email: string;
+      password: string;
+      anonymousCartId?: string;
+      activeCartSignInMode?: CartSignInModeEnum;
+      updateProductData?: boolean;
+    } = {
+      email,
+      password,
+    };
+
+    if (anonymousCartId) {
+      data.anonymousCartId = anonymousCartId;
+      data.activeCartSignInMode = CartSignInModeEnum.MergeWithExistingCustomerCart;
+      data.updateProductData = true;
+    }
+
     const response: ClientResponse<CustomerSignInResult> = await passwordFlowClient
       .me()
       .login()
       .post({
-        body: {
-          email,
-          password,
-        },
+        body: data,
       })
       .execute();
 
     if (!response.body) {
       return null;
     }
-    await getActiveCart();
+
     localStorage.removeItem(LocalStorageKeys.ANONYMOUS_CART_ID);
     localStorage.removeItem(LocalStorageKeys.ANONYMOUS_ID);
+
+    await getActiveCart();
+
     return response.body;
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
     } else {
-      throw new Error('Login failed: Unknown error');
+      throw new Error('Login failed: An unknown error occurred');
     }
   }
 }
