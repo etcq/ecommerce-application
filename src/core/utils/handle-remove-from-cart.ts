2ffc52a481a8248c7461cb3853b13d709707125a
turik777 <52492191+turik777@users.noreply.github.com>
2025-06-19 06:49:31 +0300

    feature/cart-icon-quantity (#65)
    * feat: add quantity indicator for cart icon
    
    * fix: lint error

--- src/core/utils/handle-remove-from-cart.ts
+++ src/core/utils/handle-remove-from-cart.ts
@@ -2,6 +2,7 @@ import { CartMessages } from '@/constants/constants';
 import { useToastStore } from '@/core/stores/toast.ts';
 import { removeLineItem } from '@/core/api/cart/remove-product.ts';
 import { Cart } from '@commercetools/platform-sdk';
+import { useCartStore } from '../stores/use-cart-state';
 
 interface IRemoveFN {
   cartId: string;
@@ -14,6 +15,7 @@ export default async function handleRemoveFromCart({ cartId, version, id, setCar
   try {
     const updatedCart = await removeLineItem(cartId, version, id);
     setCart(updatedCart);
+    useCartStore.getState().setLineItems(updatedCart.lineItems);
     useToastStore.getState().setMessage(CartMessages.ITEM_DELETE);
   } catch (error) {
     console.error('Failed to remove item:', error);
