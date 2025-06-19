584055fb75f34f87f8cf4d06ce8c1cbdedca70f4
turik777 <52492191+turik777@users.noreply.github.com>
2025-06-19 04:19:11 +0300

    feat: add quantity indicator for cart icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,15 @@ import { Link, NavLink } from 'react-router';
 
 import LoginMenu from './login-menu/LoginMenu';
 import logoImg from '../../assets/images/header/logo.png';
-import { JSX } from 'react';
+import { JSX, useEffect } from 'react';
 import styles from './header.module.scss';
 import { ROUTES } from '@/constants/constants';
 import BurgerMenu from './burger-menu/BurgerMenu';
 import NavigationMenu from '@components/nav-menu/NavigationMenu.tsx';
 import { useHeaderState } from '@/core/stores/state-header.ts';
+import { Badge } from '@mui/material';
+import { useCartStore } from '@/core/stores/use-cart-state';
+import { getActiveCart } from '@/core/api/cart/get-active-cart';
 
 const menuLinks = [
   { route: ROUTES.MAIN, caption: 'Home' },
@@ -17,6 +20,11 @@ const menuLinks = [
 
 export default function Header(): JSX.Element {
   const { isLoginMenuOpened, toggleLoginMenuOpened, isDarkTheme } = useHeaderState();
+  const lineItems = useCartStore((state) => state.lineItems);
+
+  useEffect(() => {
+    getActiveCart().catch((error: Error) => console.error('Failed to get cart', error.message));
+  }, []);
 
   return (
     <div className={isDarkTheme ? styles['dark-theme'] : ''}>
@@ -47,11 +55,13 @@ export default function Header(): JSX.Element {
             </div>
             <LoginMenu />
           </div>
-          <Link to={ROUTES.CART} className={styles['header__user-icon']}>
-            <svg width="19" height="20" viewBox="0 0 19 20" fill="black" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.60455 0C12.2845 0 14.4878 2.10591 14.5996 4.77432H14.5735C14.5766 4.85189 14.5617 4.92913 14.5299 5H14.6861C15.9031 5 17.1775 5.84351 17.6884 7.8798L17.744 8.12007L18.5129 14.3147C19.0662 18.2657 16.9046 19.9273 13.9558 19.9977L13.7581 20H5.46814C2.47139 20 0.162154 18.908 0.66989 14.5836L0.704514 14.3147L1.48225 8.12007C1.86575 5.92719 3.15322 5.06225 4.39373 5.00326L4.53132 5H4.6095C4.59631 4.92535 4.59631 4.84898 4.6095 4.77432C4.72133 2.10591 6.9246 0 9.60455 0ZM6.69661 8.32929C6.2085 8.32929 5.81282 8.73655 5.81282 9.23893C5.81282 9.74131 6.2085 10.1486 6.69661 10.1486C7.18471 10.1486 7.5804 9.74131 7.5804 9.23893L7.57351 9.12483C7.51897 8.67631 7.14716 8.32929 6.69661 8.32929ZM12.4854 8.32929C11.9973 8.32929 11.6016 8.73655 11.6016 9.23893C11.6016 9.74131 11.9973 10.1486 12.4854 10.1486C12.9735 10.1486 13.3692 9.74131 13.3692 9.23893C13.3692 8.73655 12.9735 8.32929 12.4854 8.32929ZM9.56536 1.30238C7.64125 1.30238 6.08145 2.85682 6.08145 4.77432C6.09463 4.84898 6.09463 4.92535 6.08145 5H13.0928C13.065 4.92794 13.0502 4.85153 13.0493 4.77432C13.0493 2.85682 11.4895 1.30238 9.56536 1.30238Z" />
-            </svg>
-          </Link>
+          <Badge badgeContent={lineItems.length} color="primary">
+            <Link to={ROUTES.CART} className={styles['header__user-icon']}>
+              <svg width="19" height="20" viewBox="0 0 19 20" fill="black" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.60455 0C12.2845 0 14.4878 2.10591 14.5996 4.77432H14.5735C14.5766 4.85189 14.5617 4.92913 14.5299 5H14.6861C15.9031 5 17.1775 5.84351 17.6884 7.8798L17.744 8.12007L18.5129 14.3147C19.0662 18.2657 16.9046 19.9273 13.9558 19.9977L13.7581 20H5.46814C2.47139 20 0.162154 18.908 0.66989 14.5836L0.704514 14.3147L1.48225 8.12007C1.86575 5.92719 3.15322 5.06225 4.39373 5.00326L4.53132 5H4.6095C4.59631 4.92535 4.59631 4.84898 4.6095 4.77432C4.72133 2.10591 6.9246 0 9.60455 0ZM6.69661 8.32929C6.2085 8.32929 5.81282 8.73655 5.81282 9.23893C5.81282 9.74131 6.2085 10.1486 6.69661 10.1486C7.18471 10.1486 7.5804 9.74131 7.5804 9.23893L7.57351 9.12483C7.51897 8.67631 7.14716 8.32929 6.69661 8.32929ZM12.4854 8.32929C11.9973 8.32929 11.6016 8.73655 11.6016 9.23893C11.6016 9.74131 11.9973 10.1486 12.4854 10.1486C12.9735 10.1486 13.3692 9.74131 13.3692 9.23893C13.3692 8.73655 12.9735 8.32929 12.4854 8.32929ZM9.56536 1.30238C7.64125 1.30238 6.08145 2.85682 6.08145 4.77432C6.09463 4.84898 6.09463 4.92535 6.08145 5H13.0928C13.065 4.92794 13.0502 4.85153 13.0493 4.77432C13.0493 2.85682 11.4895 1.30238 9.56536 1.30238Z" />
+              </svg>
+            </Link>
+          </Badge>
         </div>
         <BurgerMenu />
       </header>
